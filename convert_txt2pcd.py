618daf88a1e1b8e285ec514de2684a2dab757598
asaR1329 <ee193090@meiji.ac.jp>
2023-01-12 11:41:07 +0900

    new file

--- convert_txt2pcd.py
+++ convert_txt2pcd.py
@@ -36,7 +36,7 @@ def convert_pcd(fname):
     depthPoints, points_num = input_depth_points(ifname)
 
     ### PCDファイル作成
-    if points_num!=0 and int(fname)%10==0: #点が存在 and 0.1秒刻み
+    if points_num!=0 and int(fname)%10==0: #点が存在 and 1000/n Hzで出力
         with open(ofname, mode='w') as fp:
             fp.write('VERSION .7\n')
             fp.write('FIELDS x y z rgb\n')
@@ -45,16 +45,19 @@ def convert_pcd(fname):
 
             ###txtデータを変換，調整
             for ln in depthPoints:
-                o_data[0] = ( float(ln[2])+7  )/scale * 30  #z
-                o_data[1] = (-float(ln[0])+300)/scale       #x
-                o_data[2] = (-float(ln[1])+200)/scale       #y
-                o_data[3] = 10+float(ln[2])*0                  #RGB
+                o_data[0] = ( float(ln[2])+7  )/scale * 30      #z
+                o_data[1] = (-float(ln[0])+300)/scale           #x
+                o_data[2] = (-float(ln[1])+200)/scale           #y
+                o_data[3] = 10+float(ln[2])*0                   #RGB
                 o_dmap.append(copy.deepcopy(o_data))
 
             for ll in o_dmap:
-                print(*ll, sep=' ', file=fp)    #ファイルに出力
+                print(*ll, sep=' ', file=fp)                    #ファイルに出力
 
-        print(f'---end convert pcd {fname} {points_num}---')
+        print(f'---end convert pcd {fname} {points_num}---')    #確認用
+
+    # else:
+    #     print('points_num = ', points_num)
 ### ->pcd file
 
 ### txt files => pcd files
@@ -67,15 +70,16 @@ def convert_pcds(fname):
         ### file name 調整
         cfname = cfname[:6-len(str(ln))] + str(ln)
 
-        #なければ作成
-        try:
-            with open(cfname, mode='x') as fp:
-                # print('convert_txt2pcd:make new pcd file')
-                myfile = Path(f'output/{cfname}.pcd')
-                myfile.touch(exist_ok=True)
-        except FileExistsError:
-            pass
+        ### pcdがない場合に作成する
+        # try:
+        #     with open(f'output/{cfname}.pcd', mode='x') as fp:
+        #         print('convert_txt2pcd:remake pcd file')
+        # except FileExistsError:
+        #     pass
+
         convert_pcd(cfname)
+
+        time.sleep(0.1)
 
 def main():
     args = sys.argv
